24fa49e86041187e79783274a35092b55e432114
juankman94 <juan.carlos.menonita@gmail.com>
2019-05-22 13:03:41 -0500

    [refactor] AudioPlayer is now a class instead of a singleton

--- audioplayer.js
+++ audioplayer.js
@@ -1,25 +1,65 @@
-'use strict'
-
-const PLAY_CHARACTER = "&#x25b6;"
-const PAUSE_CHARACTER = "||"
-const INITIAL_VOLUME = 0.5
-const audioPlayer = {
+const defaultOptions = {
   controls: {
-    toggle: null,
-    prev: null,
-    next: null,
-    volume: null,
-    seek: null,
-    currentTime: null,
-    duration: null,
-    track: null,
-    artist: null,
-    title: null,
-    playlist: null,
+    toggle: document.createElement('button'),
+    prev: document.createElement('button'),
+    next: document.createElement('button'),
+    volume: document.createElement('input'),
+    seek: document.createElement('input'),
+    currentTime: document.createElement('span'),
+    duration: document.createElement('span'),
+    track: document.createElement('span'),
+    artist: document.createElement('span'),
+    title: document.createElement('span'),
+    playlist: document.createElement('ul'),
+  },
+  labels: {
+    play: '&#x25b6;',
+    pause: '||',
   },
   playlist: [],
   currentSong: 0,
   song: new Audio(),
+}
+
+const mergeStrategy = function (parentVal, childVal) {
+  return (childVal === undefined)
+    ? parentVal
+    : childVal
+}
+
+const initMixin = function (AudioPlayer) {
+  for (let k in defaultOptions) {
+    AudioPlayer.prototype[k] = defaultOptions[k]
+  }
+
+  // inspired by Vue
+  AudioPlayer.prototype._init = function(options) {
+    const ap = this
+
+    initControls(ap, options.controls || {})
+    initLabels(ap, options.labels || {})
+
+    if (options.playlist) {
+      ap.setPlaylist(options.playlist)
+    }
+    if (options.currentSong) {
+      ap.currentSong = options.currentSong
+    }
+
+    if (! (ap.song instanceof Audio)) {
+      if (ap.playlist.length > 0) {
+        ap.song = new Audio(ap.playlist[ap.currentSong].src)
+      } else {
+        ap.song = new Audio()
+      }
+    }
+
+    initListeners(ap)
+  }
+}
+
+const initProperties = function (AudioPlayer) {
+  const isPlaying = {}
 
   /**
    * Return whether the audio player is not paused
@@ -27,135 +67,146 @@ const audioPlayer = {
    * This is a virtual property that updates the toggle button
    * @return True if audio is being played, false otherwise
    */
-  get isPlaying() {
-    if (audioPlayer.song.paused) {
-      audioPlayer.controls.toggle.innerHTML = PLAY_CHARACTER
+  isPlaying.get = function() {
+    if (this.song.paused) {
+      this.controls.toggle.innerHTML = this.labels.play
       return false
     } else {
-      audioPlayer.controls.toggle.innerHTML = PAUSE_CHARACTER
+      this.controls.toggle.innerHTML = this.labels.pause
       return true
     }
+  }
+
+  Object.defineProperty(AudioPlayer.prototype, 'isPlaying', isPlaying)
+}
+
+const initControls = function (ap, newControls) {
+  for (let k in defaultOptions.controls) {
+    ap.controls[k] = mergeStrategy(defaultOptions.controls[k], newControls[k])
+  }
+}
+
+const initLabels = function (ap, newLabels) {
+  for (let k in defaultOptions.labels) {
+    ap.labels[k] = mergeStrategy(defaultOptions.labels[k], newLabels[k])
+  }
+}
+
+const initListeners = function (ap) {
+  ap.controls.toggle.addEventListener('click', ap.togglePlay.bind(ap))
+  ap.controls.next.addEventListener('click', ap.playNextSong.bind(ap))
+  ap.controls.prev.addEventListener('click', ap.playPrevSong.bind(ap))
+  ap.controls.volume.addEventListener('change', ap.setVolume.bind(ap))
+  ap.controls.seek.addEventListener('change', ap.setSongCurrentTime.bind(ap))
+
+  ap.song.addEventListener('timeupdate', ap.updateSeekBar.bind(ap))
+  ap.song.addEventListener('timeupdate', ap.updateTimestamps.bind(ap))
+  ap.song.addEventListener('ended', ap.onSongEnd.bind(ap))
+}
+
+const initMethods = function (AudioPlayer) {
+  AudioPlayer.prototype.setPlaylist = function(newPlaylist) {
+    this.playlist = newPlaylist
+    this.setSong(0)
   },
 
-  // methods 
+  AudioPlayer.prototype.setSong = function(i, forcePlay) {
+    this.song.src = this.playlist[i].src
+    this.currentSong = i
+    this.setSongInfo(i)
+    this.updateTimestamps()
+    this.refreshPlaylist()
 
-  init(newControls) {
-    return new Promise((resolve, reject) => {
-      try {
-        for (let k in audioPlayer.controls) {
-          if (newControls[k]) {
-            audioPlayer.controls[k] = newControls[k]
-          }
-        }
-      } catch (err) {
-        reject(err)
-      }
-
-      resolve(audioPlayer)
-    }).then((player) => {
-      // add event listeners
-      audioPlayer.controls.toggle.addEventListener('click', audioPlayer.togglePlay)
-      audioPlayer.controls.next.addEventListener('click', audioPlayer.playNextSong)
-      audioPlayer.controls.prev.addEventListener('click', audioPlayer.playPrevSong)
-      audioPlayer.controls.volume.addEventListener('change', audioPlayer.setVolume)
-      audioPlayer.controls.seek.addEventListener('change', audioPlayer.setSongCurrentTime)
-
-      audioPlayer.song.addEventListener('timeupdate', audioPlayer.updateSeekBar)
-      audioPlayer.song.addEventListener('timeupdate', audioPlayer.updateTimestamps)
-      audioPlayer.song.addEventListener('ended', audioPlayer.onSongEnd)
-
-      return audioPlayer
-    })
-  },
-
-  setPlaylist(newPlaylist) {
-    audioPlayer.playlist = newPlaylist
-    audioPlayer.setSong(0)
-  },
-
-  setSong(i, forcePlay) {
-    audioPlayer.song.src = audioPlayer.playlist[i].src
-    audioPlayer.currentSong = i
-    audioPlayer.setSongInfo(i)
-    audioPlayer.updateTimestamps()
-    audioPlayer.refreshPlaylist()
-
-    if (forcePlay && !audioPlayer.isPlaying) {
-      audioPlayer.song.play()
+    if (forcePlay && !this.isPlaying) {
+      this.song.play()
     }
   },
-  playNextSong() {
-    if (audioPlayer.currentSong < (playlist.length - 1)) {
-      audioPlayer.setSong(audioPlayer.currentSong + 1, true)
+
+  AudioPlayer.prototype.playNextSong = function() {
+    if (this.currentSong < (playlist.length - 1)) {
+      this.setSong(this.currentSong + 1, true)
     }
   },
-  playPrevSong() {
-    if (audioPlayer.currentSong > 0) {
-      audioPlayer.setSong(audioPlayer.currentSong - 1, true)
+
+  AudioPlayer.prototype.playPrevSong = function() {
+    if (this.currentSong > 0) {
+      this.setSong(this.currentSong - 1, true)
     }
   },
-  refreshPlaylist() {
+
+  AudioPlayer.prototype.refreshPlaylist = function() {
     let li = null
-    audioPlayer.controls.playlist.innerHTML = ""
+    this.controls.playlist.innerHTML = ""
 
-    audioPlayer.playlist.forEach((el, i) => {
+    this.playlist.forEach((el, i) => {
       li = document.createElement('li')
-      li.textContent = `${i+1} ${audioPlayer.playlist[i].artist} - ${audioPlayer.playlist[i].title}`
-      if (audioPlayer.currentSong == i) {
+      li.textContent = `${i+1} ${this.playlist[i].artist} - ${this.playlist[i].title}`
+      if (this.currentSong == i) {
         li.classList.add('active')
       }
-      audioPlayer.controls.playlist.appendChild(li)
+      this.controls.playlist.appendChild(li)
     })
   },
-  onSongEnd() {
-    if (audioPlayer.currentSong === (audioPlayer.playlist.length - 1)) {
-      audioPlayer.setSong(0, true)
+
+  AudioPlayer.prototype.onSongEnd = function() {
+    if (this.currentSong === (this.playlist.length - 1)) {
+      this.setSong(0, true)
     } else {
-      audioPlayer.setSong(audioPlayer.currentSong + 1, true)
-    }
-  },
-  setSongInfo(i) {
-    audioPlayer.controls.track.textContent = i + 1
-    audioPlayer.controls.artist.textContent = audioPlayer.playlist[i].artist
-    audioPlayer.controls.title.textContent = audioPlayer.playlist[i].title
-  },
-
-  updateSeekBar() {
-    audioPlayer.controls.seek.max = audioPlayer.song.duration
-    audioPlayer.controls.seek.value = audioPlayer.song.currentTime
-  },
-
-  updateTimestamps(ev) {
-    if (audioPlayer.song.currentTime) {
-      audioPlayer.controls.currentTime.textContent = audioPlayer.song.currentTime.toFixed()
-    }
-    if (audioPlayer.song.duration) {
-      audioPlayer.controls.duration.textContent = audioPlayer.song.duration.toFixed()
+      this.setSong(this.currentSong + 1, true)
     }
   },
 
-  setSongCurrentTime(ev) {
-    if (audioPlayer.song) {
-      audioPlayer.song.currentTime = ev.target.value
+  AudioPlayer.prototype.setSongInfo = function(i) {
+    this.controls.track.textContent = i + 1
+    this.controls.artist.textContent = this.playlist[i].artist
+    this.controls.title.textContent = this.playlist[i].title
+  },
+
+  AudioPlayer.prototype.updateSeekBar = function() {
+    this.controls.seek.max = this.song.duration
+    this.controls.seek.value = this.song.currentTime
+  },
+
+  AudioPlayer.prototype.updateTimestamps = function(ev) {
+    if (this.song.currentTime) {
+      this.controls.currentTime.textContent = this.song.currentTime.toFixed()
+    }
+    if (this.song.duration) {
+      this.controls.duration.textContent = this.song.duration.toFixed()
     }
   },
 
-  setVolume(ev) {
+  AudioPlayer.prototype.setSongCurrentTime = function(ev) {
+    if (this.song) {
+      this.song.currentTime = ev.target.value
+    }
+  },
+
+  AudioPlayer.prototype.setVolume = function(ev) {
     let val = ev.target.value
-    if (val > 1) {
+    while (val > 1) {
       val /= 100
     }
 
-    if (audioPlayer.song) {
-      audioPlayer.song.volume = val
+    if (this.song) {
+      this.song.volume = val
     }
   },
 
-  togglePlay(ev) {
-    if (audioPlayer.isPlaying) {
-      audioPlayer.song.pause()
+  AudioPlayer.prototype.togglePlay = function(ev) {
+    if (this.isPlaying) {
+      this.song.pause()
     } else {
-      audioPlayer.song.play()
+      this.song.play()
     }
   }
 }
+
+function AudioPlayer (options) {
+  options = options || {}
+  this._init(options)
+}
+
+initMixin(AudioPlayer)
+initProperties(AudioPlayer)
+initMethods(AudioPlayer)
