2c7c55ce8aa42459b0cdb505af690f26bc329348
juankman94 <juan.carlos.menonita@gmail.com>
2019-05-22 19:39:25 -0500

    Format song current time and duration

--- audioplayer.js
+++ audioplayer.js
@@ -21,13 +21,21 @@ const defaultOptions = {
   song: new Audio(),
 }
 
-const mergeStrategy = function (parentVal, childVal) {
+function pad(n, width, z) {
+  z = z || '0'
+  n = n + ''
+  return n.length >= width
+    ? n
+    : new Array(width - n.length + 1).join(z) + n
+}
+
+const mergeStrategy = function(parentVal, childVal) {
   return (childVal === undefined)
     ? parentVal
     : childVal
 }
 
-const initMixin = function (AudioPlayer) {
+const initMixin = function(AudioPlayer) {
   for (let k in defaultOptions) {
     AudioPlayer.prototype[k] = defaultOptions[k]
   }
@@ -58,7 +66,7 @@ const initMixin = function (AudioPlayer) {
   }
 }
 
-const initProperties = function (AudioPlayer) {
+const initProperties = function(AudioPlayer) {
   const isPlaying = {}
 
   /**
@@ -80,19 +88,19 @@ const initProperties = function (AudioPlayer) {
   Object.defineProperty(AudioPlayer.prototype, 'isPlaying', isPlaying)
 }
 
-const initControls = function (ap, newControls) {
+const initControls = function(ap, newControls) {
   for (let k in defaultOptions.controls) {
     ap.controls[k] = mergeStrategy(defaultOptions.controls[k], newControls[k])
   }
 }
 
-const initLabels = function (ap, newLabels) {
+const initLabels = function(ap, newLabels) {
   for (let k in defaultOptions.labels) {
     ap.labels[k] = mergeStrategy(defaultOptions.labels[k], newLabels[k])
   }
 }
 
-const initListeners = function (ap) {
+const initListeners = function(ap) {
   ap.controls.toggle.addEventListener('click', ap.togglePlay.bind(ap))
   ap.controls.next.addEventListener('click', ap.playNextSong.bind(ap))
   ap.controls.prev.addEventListener('click', ap.playPrevSong.bind(ap))
@@ -103,98 +111,117 @@ const initListeners = function (ap) {
   ap.song.addEventListener('ended', ap.onSongEnd.bind(ap))
 }
 
-const initMethods = function (AudioPlayer) {
-  AudioPlayer.prototype.setPlaylist = function(newPlaylist) {
-    this.playlist = newPlaylist
-    this.setSong(0)
-  },
+const initMethods = function(AudioPlayer) {
+  const audioPlayerMethods = {
+    setPlaylist(newPlaylist) {
+      this.playlist = newPlaylist
+      this.setSong(0)
+    },
 
-  AudioPlayer.prototype.setSong = function(i, forcePlay) {
-    this.song.src = this.playlist[i].src
-    this.currentSong = i
-    this.setSongInfo(i)
-    this.updateTimestamps()
-    this.refreshPlaylist()
+    setSong(i, forcePlay) {
+      this.song.src = this.playlist[i].src
+      this.currentSong = i
+      this.setSongInfo(i)
+      this.updateTimestamps()
+      this.refreshPlaylist()
 
-    if (forcePlay && !this.isPlaying) {
-      this.song.play()
-    }
-  },
-
-  AudioPlayer.prototype.playNextSong = function() {
-    if (this.currentSong < (playlist.length - 1)) {
-      this.setSong(this.currentSong + 1, true)
-    }
-  },
-
-  AudioPlayer.prototype.playPrevSong = function() {
-    if (this.currentSong > 0) {
-      this.setSong(this.currentSong - 1, true)
-    }
-  },
-
-  AudioPlayer.prototype.refreshPlaylist = function() {
-    let li = null
-    this.controls.playlist.innerHTML = ""
-
-    this.playlist.forEach((el, i) => {
-      li = document.createElement('li')
-      li.textContent = `${i+1} ${this.playlist[i].artist} - ${this.playlist[i].title}`
-      if (this.currentSong == i) {
-        li.classList.add('active')
+      if (forcePlay && !this.isPlaying) {
+        this.song.play()
       }
-      this.controls.playlist.appendChild(li)
-    })
-  },
+    },
 
-  AudioPlayer.prototype.onSongEnd = function() {
-    if (this.currentSong === (this.playlist.length - 1)) {
-      this.setSong(0, true)
-    } else {
-      this.setSong(this.currentSong + 1, true)
-    }
-  },
+    playNextSong() {
+      if (this.currentSong < (playlist.length - 1)) {
+        this.setSong(this.currentSong + 1, true)
+      }
+    },
 
-  AudioPlayer.prototype.setSongInfo = function(i) {
-    this.controls.track.textContent = i + 1
-    this.controls.artist.textContent = this.playlist[i].artist
-    this.controls.title.textContent = this.playlist[i].title
-  },
+    playPrevSong() {
+      if (this.currentSong > 0) {
+        this.setSong(this.currentSong - 1, true)
+      }
+    },
 
-  AudioPlayer.prototype.updateTimestamps = function(ev) {
-    if (this.song.currentTime) {
-      this.controls.seek.value = this.song.currentTime
-      this.controls.currentTime.textContent = this.song.currentTime.toFixed()
-    }
-    if (this.song.duration) {
-      this.controls.seek.max = this.song.duration
-      this.controls.duration.textContent = this.song.duration.toFixed()
-    }
-  },
+    refreshPlaylist() {
+      let li = null
+      this.controls.playlist.innerHTML = ""
 
-  AudioPlayer.prototype.setSongCurrentTime = function(ev) {
-    if (this.song) {
-      this.song.currentTime = ev.target.value
-    }
-  },
+      this.playlist.forEach((el, i) => {
+        li = document.createElement('li')
+        li.textContent = `${i+1} ${this.playlist[i].artist} - ${this.playlist[i].title}`
+        if (this.currentSong == i) {
+          li.classList.add('active')
+        }
+        this.controls.playlist.appendChild(li)
+      })
+    },
 
-  AudioPlayer.prototype.setVolume = function(ev) {
-    let val = ev.target.value
-    while (val > 1) {
-      val /= 100
-    }
+    onSongEnd() {
+      if (this.currentSong === (this.playlist.length - 1)) {
+        this.setSong(0, true)
+      } else {
+        this.setSong(this.currentSong + 1, true)
+      }
+    },
 
-    if (this.song) {
-      this.song.volume = val
-    }
-  },
+    setSongInfo(i) {
+      this.controls.track.textContent = i + 1
+      this.controls.artist.textContent = this.playlist[i].artist
+      this.controls.title.textContent = this.playlist[i].title
+    },
 
-  AudioPlayer.prototype.togglePlay = function(ev) {
-    if (this.isPlaying) {
-      this.song.pause()
-    } else {
-      this.song.play()
-    }
+    updateTimestamps(ev) {
+      const timeZero = '00:00'
+
+      if (this.song.currentTime) {
+        this.controls.seek.value = this.song.currentTime
+        this.controls.currentTime.textContent = this.formatSongTime(this.song.currentTime)
+      } else {
+        this.controls.currentTime.textContent = timeZero
+      }
+      if (this.song.duration) {
+        this.controls.seek.max = this.song.duration
+        this.controls.duration.textContent = this.formatSongTime(this.song.duration)
+      } else {
+        this.controls.duration.textContent = timeZero
+      }
+    },
+
+    setSongCurrentTime(ev) {
+      if (this.song) {
+        this.song.currentTime = ev.target.value
+      }
+    },
+
+    setVolume(ev) {
+      let val = ev.target.value
+      while (val > 1) {
+        val /= 100
+      }
+
+      if (this.song) {
+        this.song.volume = val
+      }
+    },
+
+    togglePlay(ev) {
+      if (this.isPlaying) {
+        this.song.pause()
+      } else {
+        this.song.play()
+      }
+    },
+
+    formatSongTime(t) {
+      const s = (t % 60).toFixed()
+      const m = (t / 60).toFixed()
+
+      return `${pad(m, 2)}:${pad(s,2)}`
+    },
+  }
+
+  for (let k in audioPlayerMethods) {
+    AudioPlayer.prototype[k] = audioPlayerMethods[k]
   }
 }
 
